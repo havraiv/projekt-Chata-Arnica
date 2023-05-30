77715b0e4c41fa2f49e25266ebd5f2efe0cfbb54
Iva Havranová <havranova.i@seznam.cz>
2023-05-30 19:25:03 +0200

    darkmode done

--- index.js
+++ index.js
@@ -3,12 +3,21 @@
 const toggleSwitch = document.querySelector(
   '.theme-switch input[type="checkbox"]',
 );
+const theme = localStorage.getItem('data-theme');
+if (theme === 'dark') {
+  document.documentElement.setAttribute('data-theme', 'dark');
+  document.querySelector('#checkbox').checked = true;
+} else {
+  document.documentElement.setAttribute('data-theme', 'light');
+}
 
 const switchTheme = (e) => {
   if (e.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('data-theme', 'dark');
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('data-theme', 'light');
   }
 };
 
